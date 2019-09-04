d300ee7227c5f38c44923799fc2efff244f0a5d7
liss22 <liss22@hanmail.net>
2019-09-04 23:13:33 +0900

    공사중 재공지

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -54,12 +54,12 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-statusbar": "2.4.2",
-    "cordova-plugin-device": "2.0.2",
-    "cordova-plugin-splashscreen": "5.0.2",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-statusbar": "2.4.3",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-splashscreen": "5.0.3",
     "cordova-plugin-ionic-webview": "4.1.1",
-    "cordova-plugin-ionic-keyboard": "2.1.3"
+    "cordova-plugin-ionic-keyboard": "2.2.0"
 }
 // BOTTOM OF METADATA
 });
