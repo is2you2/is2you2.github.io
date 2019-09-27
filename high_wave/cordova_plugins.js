8e5c73e7875bbae912a19e10958997a15280ff86
liss22 <liss22@hanmail.net>
2019-09-28 01:15:27 +0900

    아이콘 수정됨

--- high_wave/cordova_plugins.js
+++ high_wave/cordova_plugins.js
@@ -55,7 +55,7 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-device": "2.0.2",
+    "cordova-plugin-device": "2.0.3",
     "cordova-plugin-background-mode": "0.7.3",
     "cordova-plugin-ionic-keyboard": "2.2.0",
     "cordova-plugin-ionic-webview": "4.1.2",
