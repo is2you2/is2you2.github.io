cc357452affa0a8b16caa21b30fc9712cc8f8f14
liss22 <liss22@hanmail.net>
2020-10-19 05:21:12 +0900

    항공모함게임 링크 추가됨

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -57,7 +57,7 @@ module.exports.metadata =
 {
     "cordova-plugin-device": "2.0.3",
     "cordova-plugin-ionic-keyboard": "2.2.0",
-    "cordova-plugin-ionic-webview": "4.1.3",
+    "cordova-plugin-ionic-webview": "4.2.1",
     "cordova-plugin-statusbar": "2.4.3",
     "cordova-plugin-whitelist": "1.3.4",
     "ionic-plugin-deeplinks": "1.0.20"
