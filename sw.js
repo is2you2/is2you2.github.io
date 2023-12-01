21e6faca21d95fb0c7ae3ce7243310f468a18802
liss22 <liss22@hanmail.net>
2023-12-02 08:29:12 +0900

    리눅스 설치과정 포스트 수정

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701452998';
+const cacheName = 'chirpy-1701473283';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
