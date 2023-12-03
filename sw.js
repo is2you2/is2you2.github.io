dfc9199c55af901b976c39a4336cf4d2dcac9fb0
liss22 <liss22@hanmail.net>
2023-12-03 10:05:00 +0900

    JG1 샘플 페이지 복구 및 링링크

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701548010';
+const cacheName = 'chirpy-1701565351';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
