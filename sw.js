9d19b5e767fa30b16de7504830cfb625d95e9fe8
liss22 <liss22@hanmail.net>
2023-12-08 12:10:47 +0900

    docker 속성 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701912863';
+const cacheName = 'chirpy-1702004938';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
