a47b9df11517e2c32fbbfff95ce3188b04cae03a
liss22 <liss22@hanmail.net>
2023-12-09 09:40:40 +0900

    vncserver on docker 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702004938';
+const cacheName = 'chirpy-1702082389';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
