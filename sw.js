d9a436d1e295416ba0d451085043d76676f5305d
liss22 <liss22@hanmail.net>
2023-12-12 17:40:41 +0900

    쉬운 코딩 2 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702249761';
+const cacheName = 'chirpy-1702370399';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
