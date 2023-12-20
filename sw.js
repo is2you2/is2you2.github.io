1581f79e562c77a9437f27524c4bdb04d04894d9
liss22 <liss22@hanmail.net>
2023-12-21 02:27:57 +0900

    p5js-6 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703015675';
+const cacheName = 'chirpy-1703093245';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
