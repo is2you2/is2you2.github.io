79b8509de361686ef58222d450891e6bba9b1540
liss22 <liss22@hanmail.net>
2023-12-22 05:23:54 +0900

    what-is-class-1 post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703093245';
+const cacheName = 'chirpy-1703190209';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
