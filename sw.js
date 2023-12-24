bc96eb516afa2563ee3c9a7f4ccf3bcc4efab88b
liss22 <liss22@hanmail.net>
2023-12-24 16:28:14 +0900

    p5js-8 post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703266565';
+const cacheName = 'chirpy-1703402707';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
