90a03ed5aed03250a59dcd9247c587410f8e4501
liss22 <liss22@hanmail.net>
2023-12-28 12:01:38 +0900

    최적화 하는법 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703619262';
+const cacheName = 'chirpy-1703732456';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
