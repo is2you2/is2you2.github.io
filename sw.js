d945fc37fbd9fa73ebd80a588c801f45dfe0522d
liss22 <liss22@hanmail.net>
2023-12-29 21:26:12 +0900

    버튜얼 유튜버 설정하기 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703732456';
+const cacheName = 'chirpy-1703852743';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
