0ddfb1c42b7a9b0022a91f4ee51aebdc18bfa4d6
liss22 <liss22@hanmail.net>
2024-01-07 02:38:48 +0900

    작업물 추가

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704433218';
+const cacheName = 'chirpy-1704562704';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
