0be22e17fe302e49fda4e804d17d9e6b0a91008b
liss22 <liss22@hanmail.net>
2024-01-08 04:12:58 +0900

    포트폴리오 업데이트

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704586769';
+const cacheName = 'chirpy-1704654736';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
