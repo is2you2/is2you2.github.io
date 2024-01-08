6702ac7cfbb3809ae1d930b22d7ae0f133fef6bd
liss22 <liss22@hanmail.net>
2024-01-09 06:20:24 +0900

    작업물 이미지 라벨링

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704654736';
+const cacheName = 'chirpy-1704748801';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
