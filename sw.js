a1e6008ee4fba723d0123c3883497c1bc05514ee
liss22 <liss22@hanmail.net>
2024-02-14 09:10:54 +0900

    how-to-use-pjcone-2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707808028';
+const cacheName = 'chirpy-1707869425';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
