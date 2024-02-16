3407416d984c542670436ab8cd46210a8824db2b
liss22 <liss22@hanmail.net>
2024-02-16 12:48:44 +0900

    how to use pjcone 3 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707869425';
+const cacheName = 'chirpy-1708055258';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
