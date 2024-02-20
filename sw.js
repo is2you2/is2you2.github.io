1f88e20c67498e0b707e25a67beab000aa497e30
liss22 <liss22@hanmail.net>
2024-02-20 19:46:02 +0900

    pjcone 다운로드 링크 직접 수정: post 수정

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708055258';
+const cacheName = 'chirpy-1708425885';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
