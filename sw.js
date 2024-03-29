1eb54549da0f048b84cf1825e41d1df7cb514129
liss22 <liss22@hanmail.net>
2024-03-29 18:45:13 +0900

    pjcone 서버 구축방법 포스팅

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711694127';
+const cacheName = 'chirpy-1711705477';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
