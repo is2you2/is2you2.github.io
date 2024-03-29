c44dc9dadf3f2dfae4f55f24ce6a1a62763e24e9
liss22 <liss22@hanmail.net>
2024-03-30 06:41:19 +0900

    pjcone 사용방법 포스트 수정

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711705477';
+const cacheName = 'chirpy-1711748449';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
