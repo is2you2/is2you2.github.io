40c35d763ab227fe21b9acc39dec3c3fae501dcf
liss22 <liss22@hanmail.net>
2024-03-30 23:50:56 +0900

    pjcone-server 깃 허브 주소 추가

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711794166';
+const cacheName = 'chirpy-1711810226';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
