828316c3738b18a55ed8ee9fefe2fb01e463d035
liss22 <liss22@hanmail.net>
2024-03-30 19:03:53 +0900

    블로그 포스팅 업데이트 재탕

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711748449';
+const cacheName = 'chirpy-1711793003';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
