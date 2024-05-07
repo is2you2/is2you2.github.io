b0db95308c778c43437cf265128cb6ac0798a289
liss22 <liss22@hanmail.net>
2024-05-07 21:19:13 +0900

    블로그 글에서 /usr/local/bin:/usr/bin:/usr/local/sbin:/usr/lib/jvm/default/bin:/opt/rocm-6.0.2/bin:/opt/rocm-6.0.2/hip/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/liss22/Android/Sdk/platform-tools/ 인자 수정

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1713080919';
+const cacheName = 'chirpy-1715084317';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
