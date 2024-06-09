e9ad5562aaad1be6245fd77a0077288eaefce34e
liss22 <liss22@hanmail.net>
2024-06-09 20:46:49 +0900

    작업물 대표 이미지 수정됨

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1715084317';
+const cacheName = 'chirpy-1717933389';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
