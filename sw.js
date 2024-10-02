1872a26b90d5cf27bdc3eb91068064b6e107472f
liss22 <liss22@hanmail.net>
2024-10-02 19:52:14 +0900

    myblog 포트폴리오 버튼 우선 배치 처리

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1725961628';
+const cacheName = 'chirpy-1727866242';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
