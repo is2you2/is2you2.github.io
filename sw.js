76d8805b87a13ed177504982adc778490dc63e56
liss22 <liss22@hanmail.net>
2025-01-21 03:04:30 +0900

    myblog 작업기록 순서 변경

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1727866242';
+const cacheName = 'chirpy-1737396230';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
