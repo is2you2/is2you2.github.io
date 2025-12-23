29da95547af7b5c5e00b8e485934b5485562de85
liss22 <liss22@hanmail.net>
2025-12-23 12:33:07 +0900

    pjcone v0.93.47

--- pjcone_pwa/assets/sw.js
+++ pjcone_pwa/assets/sw.js
@@ -18,31 +18,26 @@ addEventListener('notificationclick', ev => {
                     return;
                 }
             }
-            console.log(self.registration.scope, '/', self.registration.scope.includes('/pjcone_pwa/'));
             // scope 검토 후 시작부 변경하기
             if (self.registration.scope.includes('/pjcone_pwa/'))
                 targetUrl = '/pjcone_pwa' + targetUrl;
             // 아니면 새 창 열기
-            console.log(targetUrl);
             await clients.openWindow(targetUrl);
         })()
     );
 });
 
 self.addEventListener('push', e => {
-    const data = e.data.json();
-    // 알림 지우기
-    if (data.icon == 'remove') {
-        self.registration.getNotifications({ tag: `${data.id}` }).then(notifications => {
-            notifications.forEach(n => n.close());
+    e.waitUntil((async () => {
+        const data = e.data?.json();
+        if (!data) return;
+        await self.registration.showNotification(data.title, {
+            body: data.body,
+            badge: `https://is2you2.github.io/pjcone_pwa/assets/badge/${data.icon || 'favicon'}.png`,
+            icon: `https://is2you2.github.io/pjcone_pwa/assets/icon/${data.icon || 'favicon'}.png`,
+            image: data.image,
+            tag: `${data.id}`,
+            data: data,
         });
-        // 알림 생성하기
-    } else self.registration.showNotification(data.title, {
-        body: data.body,
-        badge: `https://is2you2.github.io/pjcone_pwa/assets/badge/${data.icon || 'favicon'}.png`,
-        icon: `https://is2you2.github.io/pjcone_pwa/assets/icon/${data.icon || 'favicon'}.png`,
-        image: data.image,
-        tag: `${data.id}`,
-        data: data,
-    });
+    })());
 });
